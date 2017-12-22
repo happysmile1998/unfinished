86b7dbfa80bbc3f8e361ce21d01e87af6ace32b9
happysmile1998 <2499174276@qq.com>
2017-12-23 00:37:28 +0800

    first commit

---
+++ js/load.js
@@ -0,0 +1,5 @@
+var swiper1 = new Swiper('.swiper-container1');
+var swiper2 = new Swiper('.swiper-container2');
+var swiper3 = new Swiper('.swiper-container3');
+var swiper4 = new Swiper('.swiper-container4');
+var swiper5 = new Swiper('.swiper-container5');
