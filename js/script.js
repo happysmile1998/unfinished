2c7b39f1ab9d929943d088497f36dc7ebab24b8b
happysmile1998 <2499174276@qq.com>
2017-12-24 00:32:09 +0800

    The third commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+function an()
+{
+    var mychar=document.getElementById("A");
+    if(mychar.style.display=="block")
+    {
+        document.getElementById("A").style.display="none";
+        document.getElementById("a").style.display="block";
+    }
+    else
+    {
+        document.getElementById("a").style.display="none";
+        document.getElementById("A").style.display="block";
+    }
+}
+function bn()
+{
+    var mychar=document.getElementById("B");
+    if(mychar.style.display=="block")
+    {
+        document.getElementById("B").style.display="none";
+        document.getElementById("b").style.display="block";
+    }
+    else
+    {
+        document.getElementById("b").style.display="none";
+        document.getElementById("B").style.display="block";
+    }
+}
+function cn()
+{
+    var mychar=document.getElementById("C");
+    if(mychar.style.display=="block")
+    {
+        document.getElementById("C").style.display="none";
+        document.getElementById("c").style.display="block";
+    }
+    else
+    {
+        document.getElementById("c").style.display="none";
+        document.getElementById("C").style.display="block";
+    }
+}
+function dn()
+{
+    var mychar=document.getElementById("D");
+    if(mychar.style.display=="block")
+    {
+        document.getElementById("D").style.display="none";
+        document.getElementById("d").style.display="block";
+    }
+    else
+    {
+        document.getElementById("d").style.display="none";
+        document.getElementById("D").style.display="block";
+    }
+}
+function fn()
+{
+    var mychar=document.getElementById("F");
+    if(mychar.style.display=="block")
+    {
+        document.getElementById("F").style.display="none";
+        document.getElementById("f").style.display="block";
+    }
+    else
+    {
+        document.getElementById("f").style.display="none";
+        document.getElementById("F").style.display="block";
+    }
+} 
